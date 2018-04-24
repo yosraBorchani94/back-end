2dab81f62b4ad084a159e1239c1e68c9dc9a1239
yosraBorchani94 <yosra.borchani.lfi1@gmail.com>
2018-04-24 11:35:24 +0200

    deleted Accepted user entity and repository

--- jwt-spring-sec/src/main/java/org/sid/dao/AcceptedUserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.sid.dao;
-
-import org.sid.entities.AcceptedUsers;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AcceptedUserRepository  extends JpaRepository<AcceptedUsers, Long>,CrudRepository<AcceptedUsers, Long> {
-
-}
